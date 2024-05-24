ac516a07d6c470dc5131e5548424c9e8aa524352
Lucas Alvares <lucas.alvaress@hotmail.com>
2024-05-24 14:23:49 -0300

    ✨ users login feature

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const path = require("path");
 const http = require("http");
+const socketIO = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = socketIO(server);
 
 server.listen(3000);
 
 app.use(express.static(path.join(__dirname, "public")));
+
+let connectedUsers = [];
+
+io.on("connection", (socket) => {
+    console.log("online");
+
+    socket.on("joinRequest", (username) => {
+        socket.username = username;
+        connectedUsers.push(username);
+        console.log(connectedUsers);
+
+        socket.emit("userOk", connectedUsers);
+    });
+});
